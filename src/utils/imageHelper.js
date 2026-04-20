d3b6573f8bb0ba5340a27ee7e9a247c87753eb2e
Yash Thorat <yashthorat1912@gmail.com>
2026-04-20 18:33:16 +0530

    Fixed ImageHelper

--- src/utils/imageHelper.js
+++ src/utils/imageHelper.js
@@ -3,7 +3,12 @@ export const getImageUrl = (path) => {
     return "https://images.unsplash.com/photo-1560185007-c5ca9d2c014d";
   }
 
-  const fixedPath = path.replace(/\\/g, "/");
+  // ✅ IF CLOUDINARY URL → RETURN DIRECTLY
+  if (path.startsWith("http")) {
+    return path;
+  }
 
+  // ✅ OTHERWISE (LOCAL FILE)
+  const fixedPath = path.replace(/\\/g, "/");
   return `${process.env.REACT_APP_API_URL}/${fixedPath}`;
 };
